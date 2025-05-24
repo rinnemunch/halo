ea441ce59fcf02a3bb19ef409f876403d0797cf6
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-24 02:51:29 -0400

    Added command logging to Halo with timestamped history in halo_log.txt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
 import webbrowser
-import datetime
+from datetime import datetime
 import os
 import random
 import requests
@@ -9,6 +9,13 @@ import requests
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)
 engine.setProperty('voice', engine.getProperty('voices')[1].id)  # Optional: switch to female voice
+
+
+def log_command(user_command, halo_response):
+    with open("halo_log.txt", "a", encoding="utf-8") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"[{timestamp}] You: {user_command}\n")
+        file.write(f"[{timestamp}] Halo: {halo_response}\n\n")
 
 
 def speak(text):
@@ -37,15 +44,21 @@ def listen():
 
 def handle_command(command):
     if "youtube" in command:
-        speak("Opening YouTube.")
+        response = "Opening YouTube."
+        speak(response)
+        log_command(command, response)
         webbrowser.open("https://www.youtube.com")
 
     elif "time" in command:
         now = datetime.datetime.now().strftime("%I:%M %p")
-        speak(f"The current time is {now}.")
+        response = f"The current time is {now}."
+        speak(response)
+        log_command(command, response)
 
     elif "chrome" in command:
-        speak("Opening Google Chrome.")
+        response = "Opening Google Chrome."
+        speak(response)
+        log_command(command, response)
         os.system("start chrome")
 
     elif "joke" in command:
@@ -54,26 +67,46 @@ def handle_command(command):
             "I told my computer I needed a break, and now it won’t stop sending me Kit-Kats.",
             "What do you call 8 hobbits? A hobbyte."
         ]
-        speak(random.choice(jokes))
+        response = random.choice(jokes)
+        speak(response)
+        log_command(command, response)
 
     elif "play music" in command:
-        music_path = "C:\\Users\\fulto\\Music\\StopTheCar.mp3."
+        music_path = "C:\\Users\\fulto\\Music\\StopTheCar.mp3"
+        response = "Playing music."
+        speak(response)
+        log_command(command, response)
         os.startfile(music_path)
 
     elif "weather" in command:
-        speak("Checking the weather...")
+        response = "Checking the weather..."
+        speak(response)
         try:
             res = requests.get("https://wttr.in/?format=3")
             speak(res.text)
+            log_command(command, res.text)
         except:
-            speak("Sorry, couldn't get the weather right now.")
+            error_response = "Sorry, couldn't get the weather right now."
+            speak(error_response)
+            log_command(command, error_response)
+
+    elif "log" in command or "show log" in command:
+        response = "Showing your recent commands."
+        speak(response)
+        log_command(command, response)
+        with open("halo_log.txt", "r", encoding="utf-8") as file:
+            print(file.read())
 
     elif "exit" in command or "quit" in command:
-        speak("Goodbye.")
+        response = "Goodbye."
+        speak(response)
+        log_command(command, response)
         exit()
 
     else:
-        speak("Sorry, I don't know that command yet.")
+        response = "Sorry, I don't know that command yet."
+        speak(response)
+        log_command(command, response)
 
 
 if __name__ == "__main__":

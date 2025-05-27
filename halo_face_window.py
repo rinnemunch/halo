0c8b4abacb5dd6849a8772578623e81d96920412
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-27 11:06:33 -0400

    Fixed Cortana face window sizing and removed whitespace

--- halo_face_window.py
+++ halo_face_window.py
@@ -1,22 +1,32 @@
 from PyQt6.QtWidgets import QDialog, QLabel
 from PyQt6.QtGui import QMovie
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QSize
 import os
 
 
 class HaloFaceWindow(QDialog):
-    def __init__(self, parent=None, gif_path="Gifs/halo_face_v2.gif"):
+    def __init__(self, parent=None, gif_path="Gifs/halo-cortana.gif"):
         super().__init__(parent)
         self.setWindowTitle("Halo's Face")
-        self.setFixedSize(300, 300)
 
         print(f"[DEBUG] Looking for: {os.path.abspath(gif_path)}")
-        self.label = QLabel(self)
-        self.label.setGeometry(0, 0, 300, 300)
 
         self.movie = QMovie(gif_path)
         print(f"[DEBUG] Movie valid: {self.movie.isValid()}")
+
+        self.label = QLabel(self)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setScaledContents(True)
         self.label.setMovie(self.movie)
+
+
+        fallback_size = QSize(300, 300)
+        gif_size = self.movie.frameRect().size()
+        size = gif_size if gif_size.width() > 0 else fallback_size
+
+        self.label.resize(size)
+        self.setFixedSize(size)
+
         self.movie.start()
 
         if parent:
@@ -29,5 +39,3 @@ def show_halo_face(parent=None, gif_path="Gifs/halo-cortana.gif"):
     face_window = HaloFaceWindow(parent, gif_path)
     face_window.show()
     return face_window
-
-
